c2a2cfc6d4b1c38509e5db8afa707ada797fbf21
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-06-02 07:35:04 +0300

    Flex block

--- src/modules/showBlock.js
+++ src/modules/showBlock.js
@@ -1,13 +1,18 @@
 const showBlock = () => {
     try {
-        const jsBlock = document.getElementById('jscript');
-        jsBlock.addEventListener('click', (ev) => {
+        const jsBlock = document.getElementById('jscript'),
+        pageProof = document.getElementById('html-css');
+
+        document.addEventListener('click', (ev) => {
             ev.preventDefault();
             const target = ev.target;
-            if (target.closest('.panel-heading')){
-                const collapse = target.closest('.panel-default').children[1];
-                if (collapse){
-                    collapse.classList.toggle('in');
+            if (target.closest('#jscript') || target.closest('#html-css')){
+                if (target.closest('.panel-heading')){
+                    document.querySelector('.in').classList.remove('in');
+                    const collapse = target.closest('.panel-default').children[1];
+                    if (collapse){
+                        collapse.classList.toggle('in');
+                    }
                 }
             }
         });
